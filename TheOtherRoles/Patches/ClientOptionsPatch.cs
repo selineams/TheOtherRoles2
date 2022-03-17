77a54aa51dd62959398eead02a88a2c0dc391ba4
JustASysAdmin <97370685+JustASysAdmin@users.noreply.github.com>
2022-03-17 16:15:16 -0400

    Update ClientOptionsPatch.cs

--- TheOtherRoles/Patches/ClientOptionsPatch.cs
+++ TheOtherRoles/Patches/ClientOptionsPatch.cs
@@ -20,6 +20,8 @@ namespace TheOtherRoles.Patches
             new SelectionBehaviour("Ghosts Can See Roles", () => MapOptions.ghostsSeeRoles = TheOtherRolesPlugin.GhostsSeeRoles.Value = !TheOtherRolesPlugin.GhostsSeeRoles.Value, TheOtherRolesPlugin.GhostsSeeRoles.Value),
             new SelectionBehaviour("Show Role Summary", () => MapOptions.showRoleSummary = TheOtherRolesPlugin.ShowRoleSummary.Value = !TheOtherRolesPlugin.ShowRoleSummary.Value, TheOtherRolesPlugin.ShowRoleSummary.Value),
             new SelectionBehaviour("Show Lighter / Darker", () => MapOptions.showLighterDarker = TheOtherRolesPlugin.ShowLighterDarker.Value = !TheOtherRolesPlugin.ShowLighterDarker.Value, TheOtherRolesPlugin.ShowLighterDarker.Value),
+	    new SelectionBehaviour("Better Cursor", () => MapOptions.toggleCursor = TheOtherRolesPlugin.ToggleCursor.Value = !TheOtherRolesPlugin.ToggleCursor.Value, TheOtherRolesPlugin.ToggleCursor.Value),
+
         };
         
         private static GameObject popUp;
@@ -171,6 +173,9 @@ namespace TheOtherRoles.Patches
 
                 passiveButton.OnClick.AddListener((Action) (() =>
                 {
+		    if(info.Title == "Better Cursor") {
+                        Helpers.enableCursor(false);                  
+                    }
                     button.onState = info.OnClick();
                     button.Background.color = button.onState ? Color.green : Palette.ImpostorRed;
                 }));
