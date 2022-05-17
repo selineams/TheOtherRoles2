48db09ac5c5a5c5dfe79a26194c21fabab791f71
JustASysAdmin <jerry@hosterslice.com>
2022-05-17 15:45:18 -0400

    Okay, double equal is how you compare jerry

--- TheOtherRoles/Patches/MeetingPatch.cs
+++ TheOtherRoles/Patches/MeetingPatch.cs
@@ -561,7 +561,7 @@ namespace TheOtherRoles.Patches {
         public class BlockChatBlackmailed {
             public static bool Prefix(TextBoxTMP __instance) {
 		if (Blackmailer.blackmailed != null) {
-		  if (Blackmailer.blackmailed = PlayerControl.LocalPlayer.PlayerId && MeetingHud.Instance != null) {
+		  if (Blackmailer.blackmailed == PlayerControl.LocalPlayer && MeetingHud.Instance != null) {
                     return false;
 		  }
 		}
