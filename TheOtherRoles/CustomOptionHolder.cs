ec8faf8e1f6124b84743f7fbb7a3ab1519411ffc
Jerry <jerry@hosterslice.com>
2023-01-24 19:16:59 -0600

    Add option to disable medbay walk

--- TheOtherRoles/CustomOptionHolder.cs
+++ TheOtherRoles/CustomOptionHolder.cs
@@ -385,6 +385,8 @@ namespace TheOtherRoles {
 		
 		public static CustomOption enableCamoComms;
 		
+        public static CustomOption disableMedbayWalk;
+
         public static CustomOption restrictDevices;
         public static CustomOption disableCamsRound1;
         public static CustomOption restrictAdmin;
@@ -871,7 +873,8 @@ namespace TheOtherRoles {
 
 
 
-	enableCamoComms = CustomOption.Create(1105, Types.General, "Enable Camouflage Comms", false,  null, false);
+			enableCamoComms = CustomOption.Create(1105, Types.General, "Enable Camouflage Comms", false,  null, false);
+			disableMedbayWalk = CustomOption.Create(8847, Types.General, "Disable Medbay Walk", false, null, false);
 
             restrictDevices = CustomOption.Create(1101, Types.General, "Restrict Map Information", new string[] {"Off", "Per Round", "Per Game"},  null, false);
             restrictAdmin = CustomOption.Create(1102, Types.General, "Restrict Admin Table", 30f, 0f, 600f, 5f, restrictDevices);
--- TheOtherRoles/MapOptions.cs
+++ TheOtherRoles/MapOptions.cs
@@ -14,6 +14,8 @@ namespace TheOtherRoles{
         public static bool ghostsSeeTasks = true;
         public static bool ghostsSeeVotes = true;
         public static bool showRoleSummary = true;
+        public static bool disableMedscanWalking = false;
+
         public static bool allowParallelMedBayScans = false;
         public static bool showLighterDarker = true;
         public static bool toggleCursor = true;
@@ -57,6 +59,7 @@ namespace TheOtherRoles{
             allowParallelMedBayScans = CustomOptionHolder.allowParallelMedBayScans.getBool();
             shieldFirstKill = CustomOptionHolder.shieldFirstKill.getBool();
             disableCamsRoundOne = CustomOptionHolder.disableCamsRound1.getBool();
+            disableMedscanWalking = CustomOptionHolder.disableMedbayWalk.getBool();
 			isRoundOne = true; 
             firstKillPlayer = null;
             restrictDevices = CustomOptionHolder.restrictDevices.getSelection();
